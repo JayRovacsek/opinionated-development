8db642e5d83f9f2c991cd29abbd6ac61941b3b5b
jayrovacsek <jay@rovacsek.com>
2024-03-16 14:25:14 +1100

    fix(flake): add standard-outputs to flake to resolve github workflow issue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ### ⛰️  Features
 
+- *(docs)* Add changelog to repo - ([aa3f791](https://github.com/JayRovacsek/vulnix-pre-commit/commit/aa3f7914cd009ec4ce530e1e3d7c2d20d3a50891))
+
 - *(flake)* Implement basis of the flake - ([a2f393b](https://github.com/JayRovacsek/vulnix-pre-commit/commit/a2f393b340a3602c2867a81cd2682c30af1f5963))
 
 - *(packages)* Add git check workflow configuration - ([b69e45f](https://github.com/JayRovacsek/vulnix-pre-commit/commit/b69e45f2fa2d30054c178e71b2107474636a0754))
@@ -18,6 +20,8 @@
 
 - *(packages)* Add git-cliff config - ([3385bc7](https://github.com/JayRovacsek/vulnix-pre-commit/commit/3385bc7ae36c7b45ede647bbc083b63760d100e7))
 
+- *(tooling)* Add checks configuration - ([aa3f791](https://github.com/JayRovacsek/vulnix-pre-commit/commit/aa3f7914cd009ec4ce530e1e3d7c2d20d3a50891))
+
 - *(tooling)* Add conform config - ([af7e8af](https://github.com/JayRovacsek/vulnix-pre-commit/commit/af7e8af7bb576e91e0aeccd58458cc78ea0dbeab))
 
 - *(tooling)* Add envrc - ([3b7cd9c](https://github.com/JayRovacsek/vulnix-pre-commit/commit/3b7cd9cc411dd3fe5c721cc14332756102a1e472))
@@ -25,6 +29,12 @@
 - *(tooling)* Add gitignore - ([4e010b8](https://github.com/JayRovacsek/vulnix-pre-commit/commit/4e010b832129e52cd8e035ef865325c9e783288d))
 
 ### 🐛 Bug Fixes
+
+- *(flake)* Resolve duplicated inputs - ([aa3f791](https://github.com/JayRovacsek/vulnix-pre-commit/commit/aa3f7914cd009ec4ce530e1e3d7c2d20d3a50891))
+
+- *(package)* Resolve github checks value for pull_request - ([aa3f791](https://github.com/JayRovacsek/vulnix-pre-commit/commit/aa3f7914cd009ec4ce530e1e3d7c2d20d3a50891))
+
+- *(package)* Resolve prettier modifying the checks file - ([aa3f791](https://github.com/JayRovacsek/vulnix-pre-commit/commit/aa3f7914cd009ec4ce530e1e3d7c2d20d3a50891))
 
 - *(tooling)* Add result to gitignore - ([4525811](https://github.com/JayRovacsek/vulnix-pre-commit/commit/452581189eebe8f45329e97c139524a2da407994))
 
--- flake.nix
+++ flake.nix
@@ -27,123 +27,130 @@
   };
 
   outputs = { flake-utils, nixpkgs, pre-commit-hooks, self, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = self;
-            hooks = {
-              actionlint.enable = true;
-              conform.enable = true;
-              deadnix.enable = true;
-              nixfmt.enable = true;
-              prettier.enable = true;
-              statix.enable = true;
-              typos.enable = true;
+    let
+      inherit (nixpkgs) lib;
 
-              # Custom hooks
-              conform-config = {
-                enable = true;
-                name = "Conform Config";
-                entry = ''
-                  ${pkgs.coreutils}/bin/cp ${
-                    self.packages.${system}.conform-config
-                  } .conform.yaml
-                '';
-                language = "system";
-                pass_filenames = false;
+      flake-utils-output = flake-utils.lib.eachDefaultSystem (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          checks = {
+            pre-commit-check = pre-commit-hooks.lib.${system}.run {
+              src = self;
+              hooks = {
+                actionlint.enable = true;
+                conform.enable = true;
+                deadnix.enable = true;
+                nixfmt.enable = true;
+                prettier.enable = true;
+                statix.enable = true;
+                typos.enable = true;
+
+                # Custom hooks
+                conform-config = {
+                  enable = true;
+                  name = "Conform Config";
+                  entry = ''
+                    ${pkgs.coreutils}/bin/cp ${
+                      self.packages.${system}.conform-config
+                    } .conform.yaml
+                  '';
+                  language = "system";
+                  pass_filenames = false;
+                };
+
+                github-checks-workflow = {
+                  enable = true;
+                  name = "Github Checks Workflow";
+                  entry = ''
+                    ${pkgs.coreutils}/bin/cp ${
+                      self.packages.${system}.git-workflow-checks
+                    } .github/workflows/checks.yaml
+                  '';
+                  language = "system";
+                  pass_filenames = false;
+                };
+
+                git-cliff = {
+                  enable = true;
+                  name = "Git Cliff";
+                  entry = "${pkgs.git-cliff}/bin/git-cliff --config ${
+                      self.packages.${system}.cliff-config
+                    } --output CHANGELOG.md";
+                  language = "system";
+                  pass_filenames = false;
+                };
+
+                statix-write = {
+                  enable = true;
+                  name = "Statix Write";
+                  entry = "${pkgs.statix}/bin/statix fix";
+                  language = "system";
+                  pass_filenames = false;
+                };
+
+                trufflehog-verified = {
+                  enable = true;
+                  name = "Trufflehog Search";
+                  entry =
+                    "${pkgs.trufflehog}/bin/trufflehog git file://. --since-commit HEAD --only-verified --fail --no-update";
+                  language = "system";
+                  pass_filenames = false;
+                };
+
+                trufflehog-regex = {
+                  enable = true;
+                  name = "Trufflehog Regex Search";
+                  entry =
+                    "${pkgs.trufflehog}/bin/trufflehog git file://. --since-commit HEAD --config ${
+                      self.packages.${system}.trufflehog-config
+                    } --fail --no-verification -x ${
+                      self.packages.${system}.trufflehog-ignore
+                    }  --no-update";
+                  language = "system";
+                  pass_filenames = false;
+                };
               };
 
-              github-checks-workflow = {
-                enable = true;
-                name = "Github Checks Workflow";
-                entry = ''
-                  ${pkgs.coreutils}/bin/cp ${
-                    self.packages.${system}.git-workflow-checks
-                  } .github/workflows/checks.yaml
-                '';
-                language = "system";
-                pass_filenames = false;
-              };
-
-              git-cliff = {
-                enable = true;
-                name = "Git Cliff";
-                entry = "${pkgs.git-cliff}/bin/git-cliff --config ${
-                    self.packages.${system}.cliff-config
-                  } --output CHANGELOG.md";
-                language = "system";
-                pass_filenames = false;
-              };
-
-              statix-write = {
-                enable = true;
-                name = "Statix Write";
-                entry = "${pkgs.statix}/bin/statix fix";
-                language = "system";
-                pass_filenames = false;
-              };
-
-              trufflehog-verified = {
-                enable = true;
-                name = "Trufflehog Search";
-                entry =
-                  "${pkgs.trufflehog}/bin/trufflehog git file://. --since-commit HEAD --only-verified --fail --no-update";
-                language = "system";
-                pass_filenames = false;
-              };
-
-              trufflehog-regex = {
-                enable = true;
-                name = "Trufflehog Regex Search";
-                entry =
-                  "${pkgs.trufflehog}/bin/trufflehog git file://. --since-commit HEAD --config ${
-                    self.packages.${system}.trufflehog-config
-                  } --fail --no-verification -x ${
-                    self.packages.${system}.trufflehog-ignore
-                  }  --no-update";
-                language = "system";
-                pass_filenames = false;
-              };
-            };
-
-            settings = {
-              deadnix.edit = true;
-              nixfmt.width = 80;
-              prettier = {
-                ignore-path = [ self.packages.${system}.prettier-ignore ];
-                write = true;
-              };
-              typos = {
-                configPath = "${self.packages.${system}.typos-config}";
-                locale = "en-au";
+              settings = {
+                deadnix.edit = true;
+                nixfmt.width = 80;
+                prettier = {
+                  ignore-path = [ self.packages.${system}.prettier-ignore ];
+                  write = true;
+                };
+                typos = {
+                  configPath = "${self.packages.${system}.typos-config}";
+                  locale = "en-au";
+                };
               };
             };
           };
-        };
 
-        devShells.default = pkgs.mkShell {
-          name = "opinionated-development-dev-shell";
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-        };
+          devShells.default = pkgs.mkShell {
+            name = "opinionated-development-dev-shell";
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+          };
 
+          packages = {
+            cliff-config = pkgs.callPackage ./packages/cliff-config { };
+            conform-config = pkgs.callPackage ./packages/conform-config { };
+            git-workflow-checks =
+              pkgs.callPackage ./packages/git-workflow-checks { };
+            prettier-ignore = pkgs.callPackage ./packages/prettier-ignore { };
+            trufflehog-config =
+              pkgs.callPackage ./packages/trufflehog-config { };
+            trufflehog-ignore =
+              pkgs.callPackage ./packages/trufflehog-ignore { };
+            typos-config = pkgs.callPackage ./packages/typos-config { };
+          };
+        });
+
+      standard-output = {
         githubActions = self.inputs.nix-github-actions.lib.mkGithubMatrix {
-          checks =
-            (pkgs.lib.getAttrs [ "x86_64-linux" "x86_64-darwin" ] self.checks)
-            // (pkgs.lib.getAttrs [ "x86_64-linux" "x86_64-darwin" ]
-              self.packages);
+          checks = (lib.getAttrs [ "x86_64-linux" "x86_64-darwin" ] self.checks)
+            // (lib.getAttrs [ "x86_64-linux" "x86_64-darwin" ] self.packages);
         };
+      };
 
-        packages = {
-          cliff-config = pkgs.callPackage ./packages/cliff-config { };
-          conform-config = pkgs.callPackage ./packages/conform-config { };
-          git-workflow-checks =
-            pkgs.callPackage ./packages/git-workflow-checks { };
-          prettier-ignore = pkgs.callPackage ./packages/prettier-ignore { };
-          trufflehog-config = pkgs.callPackage ./packages/trufflehog-config { };
-          trufflehog-ignore = pkgs.callPackage ./packages/trufflehog-ignore { };
-          typos-config = pkgs.callPackage ./packages/typos-config { };
-        };
-      });
+    in flake-utils-output // standard-output;
 }
